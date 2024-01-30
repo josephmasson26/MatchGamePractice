272451fa7bee9cda3f17df9c178ca28dfab06c02
josephmasson26 <c.p.joseph18@gmail.com>
2024-01-30 18:43:00 -0500

    Initial Commit

---
+++ MatchGamePractice/MatchGamePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchGamePracticeApp.swift
+//  MatchGamePractice
+//
+//  Created by Joseph Masson on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MatchGamePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
