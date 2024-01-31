0c7ddd8e54c70610213c73ac254dbda11eab87a0
josephmasson26 <c.p.joseph18@gmail.com>
2024-01-30 19:29:19 -0500

    add new structs and finalize UI

--- MatchGamePractice/ContentView.swift
+++ MatchGamePractice/ContentView.swift
@@ -11,12 +11,33 @@ struct ContentView: View {
     @State var fruits : [String] = ["🍎", "🍐", "🍊", "🍋", "🍌", "🍉", "🍇", "🍓"]
     @State var color: Color = .green
     
+    
     var body: some View {
-        CardView(content: "abcd", color: color)
-        CardView(content: "content", color: color)
+        VStack{
+            //CardView(content: "abcd", color: color)
+            //CardView(content: "content", color: color)
+            ScrollView(.vertical) {
+                cards
+            }.scrollIndicators(.hidden)
+            
+            HStack{
+                Button(action: {fruits.shuffle()}, label:{
+                    Image(systemName: "shuffle")
+                    })
+                ColorPicker(selection: $color, label: {})
+            }
+        }
+        .animation(.spring(), value: fruits)
+        .padding(16.0)
         
-        ColorPicker(selection: $color, label: {})
-
+        
+    }
+    var cards:some View {
+        LazyVGrid(columns: [GridItem(), GridItem()]) {
+            ForEach(fruits, id: \.self) {
+                fruit in CardView(content: fruit, color: color).aspectRatio(2/3, contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+            }
+        }
     }
     
 }
