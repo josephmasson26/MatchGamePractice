a4fcef00ab91a356b90722986937c93256a6ac82
josephmasson26 <c.p.joseph18@gmail.com>
2024-01-30 19:06:00 -0500

    basic view implementations with struct

--- MatchGamePractice/ContentView.swift
+++ MatchGamePractice/ContentView.swift
@@ -8,15 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var fruits : [String] = ["🍎", "🍐", "🍊", "🍋", "🍌", "🍉", "🍇", "🍓"]
+    @State var color: Color = .green
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        CardView(content: "abcd", color: color)
+        CardView(content: "content", color: color)
+        
+        ColorPicker(selection: $color, label: {})
+
     }
+    
+}
+
+struct CardView: View {
+    var content: String
+    var color: Color
+    
+    @State var isFaceUp: Bool = true
+
+    
+    var body: some View {
+    
+        ZStack{
+            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/).foregroundColor(.white)
+            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/).strokeBorder(lineWidth: 5.0).foregroundColor(color)
+            Text(content).font(.largeTitle)
+            RoundedRectangle(cornerRadius: 25.0).foregroundColor(color).opacity(isFaceUp ? 0 : 1)
+        }.onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+    
 }
 
 #Preview {
